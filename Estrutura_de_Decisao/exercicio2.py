1b96d6c7e3d1f153261b9ecaf5e05d38c72a77f9
Helfstein-one <mhgdidi@gmail.com>
2021-02-23 14:26:08 -0300

    Condicionais verdadeiro e falso

---
+++ Estrutura_de_Decisao/exercicio2.py
@@ -0,0 +1,9 @@
+a = 4
+b = 10
+c = 50
+d = 1
+f = 5
+if (c<=f):
+    print('true')
+else:
+    print("False")
---
+++ Estrutura_de_Decisao/exercicio3.py
@@ -0,0 +1,4 @@
+if ((5 > 3) or (4 < 2)) and (1 < 7):
+    print('true')
+else:
+    print('False')
