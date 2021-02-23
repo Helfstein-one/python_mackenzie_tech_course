90197e14ceca54917b01a9fbfe2fdf3cae7be057
Helfstein-one <mhgdidi@gmail.com>
2021-02-23 16:37:12 -0300

    Faça um programa em Python que, dada a idade de um atleta que será digitada pelo usuário, apresente a mensagem da coluna CATEGORIA de acordo com a tabela abaixo, onde a categoria depende da idade de entrada. Lembre-se de exibir a Categoria, exatamente como consta na tabela abaixo.
    CATEGORIA
    
    IDADE
    
    NÃO TEM IDADE PARA SER ATLETA - Menor que 5
    
    INFANTIL A - 5 a 7
    
    INFANTIL B - 8 a 10
    
    JUVENIL A - 11 a 13
    
    JUVENIL B - 14 a 17
    
    SÊNIOR - Maior ou igual a 18

---
+++ Estrutura_Condicional_Encadeada/exercicio1.py
@@ -0,0 +1,14 @@
+n1=int(input())
+if (n1 < 5):
+    print("NÃO TEM IDADE PARA SER ATLETA")
+elif ((n1 >= 5) and (n1 <= 7)):
+    print('INFANTIL A')
+elif ((n1 >= 8) and (n1 <= 10)):
+    print('INFANTIL B')
+elif ((n1 >= 11) and (n1 <= 13)):
+    print('JUVENIL A')
+elif ((n1 >= 14) and (n1 <= 17)):
+    print('JUVENIL B')
+elif (n1>18) or (n1==18):
+    print("SÊNIOR")
+
