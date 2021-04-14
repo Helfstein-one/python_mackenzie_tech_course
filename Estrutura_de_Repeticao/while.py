8f333207cadcc0590233b8f3dc7273b3ae75b6f5
Helfstein-one <mhgdidi@gmail.com>
2021-04-13 22:51:04 -0300

    Mackenzie
    Faça um programa em Python que solicite a quantidade de alunos de uma turma.
    
    Após esta informação, o usuário deve digitar a média de cada aluno da turma, e o programa apresentará a mensagem PARABÉNS VOCÊ ESTÁ APROVADO aos aluno com média maior ou igual a 6,0.
    
    O programa deve calcular e mostrar, no final da entrada de dados, a média geral da turma.
    
    Obs.: Caso a quantidade informada de alunos da turma for igual a zero, o programa deve emitir a seguinte mensagem: NÃO HOUVE PROCESSAMENTO.

---
+++ Estrutura_de_Repeticao/while.py
@@ -0,0 +1,25 @@
+numero_de_notas=int(input())
+if numero_de_notas==0:
+    print("NÃO HOUVE PROCESSAMENTO")
+else:
+    cont=0
+    total_notas=0
+    while cont<numero_de_notas:
+        nota = float(input())
+        total_notas += nota
+        cont= cont + 1
+        if nota>=6.0:
+            print("PARABÉNS VOCÊ ESTÁ APROVADO")
+    print((float(total_notas/numero_de_notas)))
+
+
+
+
+
+
+
+
+
+
+
+
